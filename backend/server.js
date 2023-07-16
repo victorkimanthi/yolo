ba68aec123ffdd5cba11bb002f42761f36226668
victor kimanthi <victorkimanthi556@gmail.com>
2023-07-16 14:37:01 +0300

    updated the server.js file by updating the mongodb url

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,8 @@ const upload = multer();
 const productRoute = require('./routes/api/productRoute');
 
 // Connecting to the Database
-let mongodb_url = 'mongodb://localhost/';
+// let mongodb_url = 'mongodb://localhost/';
+let mongodb_url = 'mongodb://mongodb:27017/';
 let dbName = 'yolomy';
 
 // define a url to connect to the database
